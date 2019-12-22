99f9bfb2dbe3756f46e28447f257f664716abcb4
JasonFaas <5844854+JasonFaas@users.noreply.github.com>
2019-12-22 15:56:13 -0800

    Major changes, still evolving

---
+++ python_src/helper.py
@@ -0,0 +1,62 @@
+import jieba
+import pinyin
+
+from googletrans import Translator
+
+
+class Helper(object):
+
+    def __init__(self):
+        self.googletrans_translator = Translator()
+
+    def other_file(self, import_str):
+        return '{} World'.format(import_str)
+
+    def pinyin_from_hanzi(self, row):
+        nh2 = row['Hanzi']
+
+        seg_list = jieba.cut(nh2)
+        what = " ".join(seg_list)
+        hanzi_arr = what.split(" ")
+        pinyin_arr = pinyin.get(nh2, delimiter=" ").split(" ")
+
+        pinyin_itr = 0
+        pinyin_final = ""
+        for hanzi_group in hanzi_arr:
+            pinyin_group = pinyin_arr[pinyin_itr:pinyin_itr + len(hanzi_group)]
+            pinyin_corr = ""
+            for pinyin_char in pinyin_group:
+                pinyin_corr += pinyin_char
+            pinyin_final += pinyin_corr + " "
+            pinyin_itr += len(hanzi_group)
+
+        return pinyin_final.strip()
+
+    def hanzi_with_spaces(self, row):
+        nh2 = row['Hanzi'].replace(' ', '')
+
+        seg_list = jieba.cut(nh2)
+        what = " ".join(seg_list)
+        return what
+
+    def pinyin_from_hanzi_googletrans(self, row):
+        hanzi = row['Hanzi'].replace(' ', '')
+        gt_translation = self.googletrans_translator.translate(hanzi, src='zh-cn', dest='en')
+        print(gt_translation, flush=True)
+        print(gt_translation.extra_data, flush=True)
+        # print(gt_translation.origin, ' -> ', gt_translation.text, flush=True)
+        translation_return = gt_translation.extra_data['translation'][1][-1].lower()
+        print(gt_translation.origin, ' -> ', translation_return)
+        print('\n', flush=True)
+
+        return translation_return
+
+    def verify_no_new_old_duplicates(self, df_1, df_2):
+        set_1 = set(df_1['Hanzi'].tolist())
+        set_2 = set(df_2['Hanzi'].tolist())
+
+        if len(set_1.intersection(set_2)) > 0:
+            print('\n\nERROR_ERROR_ERROR:\nNew and old sets should not overlap')
+            print(set_1)
+            print(set_2)
+            exit(0)
--- python_src/main_create_sql_from_output_and_input.py
+++ python_src/main_create_sql_from_output_and_input.py
@@ -1,96 +1,57 @@
 import csv
 import sqlite3
 import os
-import pinyin
-
-import jieba
 
 import pandas as pd
 
 import sys
 
+from python_src.helper import Helper
+
 print(sys.version)
 
-def pinyin_from_hanzi(row):
-    nh2 = row['Hanzi']
+# TODO: Delete verification of helper file
+helper = Helper()
+print(helper.other_file('Verification of Helper File'))
 
-    seg_list = jieba.cut(nh2)
-    what = " ".join(seg_list)
-    hanzi_arr = what.split(" ")
-    pinyin_arr = pinyin.get(nh2, delimiter=" ").split(" ")
+data_location = '../data_raw'
+empty_csv = '{}/empty.csv'.format(data_location)
+input_path = '{}/input/'.format(data_location)
+output_path = '{}/output/'.format(data_location)
 
-    pinyin_itr = 0
-    pinyin_final = ""
-    for hanzi_group in hanzi_arr:
-        pinyin_group = pinyin_arr[pinyin_itr:pinyin_itr + len(hanzi_group)]
-        pinyin_corr = ""
-        for pinyin_char in pinyin_group:
-            pinyin_corr += pinyin_char
-        pinyin_final += pinyin_corr + " "
-        pinyin_itr += len(hanzi_group)
+# print list of files in input
+input_file_names = os.listdir(input_path)
+input_file_names.sort()
 
-    return pinyin_final.strip()
+for filename in input_file_names:
+    input_csv_filename = '{}{}'.format(input_path, filename)
+    output_csv_filename = '{}{}'.format(output_path, filename)
 
-def hanzi_with_spaces(row):
-    nh2 = row['Hanzi'].replace(' ', '')
+    # read in known output
+    df_new_input = pd.read_csv(input_csv_filename)
+    if os.path.isfile(output_csv_filename):
+        df_existing_output = pd.read_csv(output_csv_filename)
+    else:
+        df_existing_output = pd.read_csv()
 
-    seg_list = jieba.cut(nh2)
-    what = " ".join(seg_list)
-    return what
+    # verify no new input conflicts
+    helper.verify_no_new_old_duplicates(df_new_input, df_existing_output)
 
+    # get info for new input to output
+    df_new_input['Pinyin'] = df_new_input.apply(lambda row: helper.pinyin_from_hanzi_googletrans(row), axis=1)
+    df_new_input['Hanzi'] = df_new_input.apply(lambda row: helper.hanzi_with_spaces(row), axis=1)
 
-from googletrans import Translator
-googletrans_translator = Translator()
-def pinyin_from_hanzi_googletrans(row):
-    global googletrans_translator
+    df_merged = pd.concat([df_existing_output, df_new_input], ignore_index=True)
+    print(df_merged)
 
-    hanzi = row['Hanzi'].replace(' ', '')
-    gt_translation = googletrans_translator.translate(hanzi, src='zh-cn', dest='en')
-    print(gt_translation, flush=True)
-    print(gt_translation.extra_data, flush=True)
-    # print(gt_translation.origin, ' -> ', gt_translation.text, flush=True)
-    translation_return = gt_translation.extra_data['translation'][1][-1].lower()
-    print(gt_translation.origin, ' -> ', translation_return)
-    print('\n', flush=True)
+    # update output file
+    df_merged.to_csv(output_csv_filename, index=False)
 
-    return translation_return
+    # remove all data from input file
+    df_empty = df_new_input[0:0]
+    df_empty.to_csv(input_csv_filename, index=False)
 
-
-def verify_no_new_old_duplicates(df_1, df_2):
-    set_1 = set(df_1['Hanzi'].tolist())
-    set_2 = set(df_2['Hanzi'].tolist())
-
-    if len(set_1.intersection(set_2)) > 0:
-        print('\n\nERROR_ERROR_ERROR:\nNew and old sets should not overlap')
-        print(set_1)
-        print(set_2)
-        exit(0)
-
-
-output_csv_filename = '../raw_data/output.csv'
-input_csv_filename = '../raw_data/input.csv'
-
-# read in known output
-df_existing_output = pd.read_csv(output_csv_filename)
-df_new_input = pd.read_csv(input_csv_filename)
-
-# verify no new input conflicts
-verify_no_new_old_duplicates(df_new_input, df_existing_output)
-
-# get info for new input to output
-
-df_new_input['Pinyin'] = df_new_input.apply(lambda row: pinyin_from_hanzi_googletrans(row), axis=1)
-df_new_input['Hanzi'] = df_new_input.apply(lambda row: hanzi_with_spaces(row), axis=1)
-
-df_merged = pd.concat([df_existing_output, df_new_input], ignore_index=True)
-print(df_merged)
-
-# update output file
-df_merged.to_csv(output_csv_filename, index=False)
-
-# remove all data from input file
-df_empty = df_new_input[0:0]
-df_empty.to_csv(input_csv_filename, index=False)
+exit(66)
 
 # create database from output
 database_name = "../sql/first.sqlite3"
@@ -98,10 +59,13 @@ os.remove(database_name)
 
 con = sqlite3.connect(database_name)
 cur = con.cursor()
-columns = ('''Hanzi''',
-           '''English''',
-           '''Difficulty''',
-           '''Pinyin''')
+columns = (
+    '''fk''',
+    '''Hanzi''',
+    '''English''',
+    '''Difficulty''',
+    '''Pinyin'''
+)
 cur.execute('''CREATE TABLE Translations
              (
                  [id] INTEGER PRIMARY KEY,
