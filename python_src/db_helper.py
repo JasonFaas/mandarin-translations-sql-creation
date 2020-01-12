6b46f1903b34292886e8ae2bcd64b60ca085d54a
JasonFaas <5844854+JasonFaas@users.noreply.github.com>
2020-01-11 22:51:22 -0800

    Refactor to add new HSK table

---
+++ python_src/db_helper.py
@@ -0,0 +1,107 @@
+import csv
+import os
+
+
+class DbHelper(object):
+    def __init__(self, cur, OUTPUT_PATH, COLUMNS, FK_PARENT, MANUAL_LEVEL, AUTO_LEVEL, BLANKS, ENGLISH, HANZI, PINYIN):
+        self.cur = cur
+        self.OUTPUT_PATH = OUTPUT_PATH
+        self.COLUMNS = COLUMNS
+        self.FK_PARENT = FK_PARENT
+        self.MANUAL_LEVEL = MANUAL_LEVEL
+        self.AUTO_LEVEL = AUTO_LEVEL
+        self.BLANKS = BLANKS
+        self.ENGLISH = ENGLISH
+        self.HANZI = HANZI
+        self.PINYIN = PINYIN
+
+        self.COLUMNS_QUESTION_MARKS = ", ".join(['?'] * len(self.COLUMNS))
+        self.INSERT_BASE = "INSERT INTO {{}} {} VALUES ( {} )".format(str(self.COLUMNS), self.COLUMNS_QUESTION_MARKS)
+
+    def writeTranslations(self):
+        output_file_names = os.listdir(self.OUTPUT_PATH)
+        output_file_names.sort()
+
+        db_in_mem = {}
+
+        for file_idx, filename in enumerate(output_file_names):
+            is_input_to_primary_table = filename[0] == '0'
+            if is_input_to_primary_table:
+                table_name = 'translations'
+            else:
+                table_name = filename[filename.index('_') + 1:filename.index('.')]
+
+            output_csv_filename = '{}{}'.format(self.OUTPUT_PATH, filename)
+            with open(output_csv_filename, 'rt') as fin:  # `with` statement available in 2.5+
+                # csv.DictReader uses first line in file for column headings by default
+                dr = csv.DictReader(fin)  # comma is default delimiter
+                to_db = [
+                    (
+                        i[('%s' % self.COLUMNS[0])],
+                        i[('%s' % self.COLUMNS[1])],
+                        i[('%s' % self.COLUMNS[2])],
+                        i[('%s' % self.COLUMNS[3])],
+                        i[('%s' % self.COLUMNS[4])],
+                        i[('%s' % self.COLUMNS[5])],
+                        i[('%s' % self.COLUMNS[6])],
+                    )
+                    for i in dr]
+
+            if not is_input_to_primary_table:
+                db_in_mem[table_name] = {}
+                for idx, row in enumerate(to_db):
+                    english_value = row[self.COLUMNS.index(self.ENGLISH)]
+                    db_in_mem[table_name][english_value] = idx
+
+            fk_parent = to_db[0][0]
+            no_fk_ref = fk_parent == "" or fk_parent == '-1'
+            if no_fk_ref:
+                table_contents_fk_ref = ''
+                for idx in range(len(to_db)):
+                    to_db[idx] = (-1,) + to_db[idx][1:]
+            else:
+                foreign_key_table_name = fk_parent[0:fk_parent.index('.')]
+                table_contents_fk_ref = ',FOREIGN KEY({}) REFERENCES {}(id)'.format(self.FK_PARENT, foreign_key_table_name)
+                for idx in range(len(to_db)):
+                    ref_split = str(to_db[idx][0]).split('.')
+                    to_db[idx] = (db_in_mem[ref_split[0]][ref_split[1]] + 1,) + to_db[idx][1:]
+
+            if file_idx == 0 or not is_input_to_primary_table:
+
+                # TODO: Be careful here with `self.COLUMNS[:3]:`
+                # TODO: Clean this up!!!
+
+                table_cont_new = '('
+                table_cont_new += '[id] INTEGER PRIMARY KEY,'
+                for col in self.COLUMNS[:3]:
+                    table_cont_new += '[{}] INTEGER,'.format(col)
+                for col in self.COLUMNS[3:-1]:
+                    table_cont_new += '[{}] text,'.format(col)
+                for col in self.COLUMNS[-1:]:
+                    table_cont_new += '[{}] text'.format(col)
+                table_cont_new += table_contents_fk_ref
+                table_cont_new += ')'
+
+                contents = '''CREATE TABLE ''' + table_name + ''' ''' + table_cont_new
+                self.cur.execute(contents)
+
+            insert_str = self.INSERT_BASE.format(table_name)
+
+            print(insert_str)
+            print(to_db)
+
+            self.cur.executemany(insert_str, to_db)
+
+    def writeHskTable(self):
+        table_name = '''hsk_info'''
+
+        hsk_table_creation = '''CREATE TABLE'''
+        hsk_table_creation += ''' '''
+        hsk_table_creation += table_name
+        hsk_table_creation += ''' '''
+        hsk_table_creation += '''([id] INTEGER PRIMARY KEY,[fk_parent] INTEGER,[Manual_Level] INTEGER,[Auto_Level] INTEGER,[Blanks] text,[English] text,[Hanzi] text,[Pinyin] text)'''
+        self.cur.execute(hsk_table_creation)
+
+        insert_str = self.INSERT_BASE.format(table_name)
+        
+        self.cur.executemany(insert_str, to_db)
--- python_src/main_create_sql_from_output_and_input.py
+++ python_src/main_create_sql_from_output_and_input.py
@@ -1,4 +1,3 @@
-import csv
 import sqlite3
 import os
 import time
@@ -8,25 +7,26 @@ import pandas as pd
 import sys
 
 from python_src.io_helper import IoHelper
+from python_src.db_helper import DbHelper
 
 print(sys.version)
 
-columns = (
-    '''fk_parent''',
-    '''Manual_Level''',
-    '''Auto_Level''',
-    '''Blanks''',
-    '''English''',
-    '''Hanzi''',
-    '''Pinyin''',
+FK_PARENT = '''fk_parent'''
+MANUAL_LEVEL = '''Manual_Level'''
+AUTO_LEVEL = '''Auto_Level'''
+BLANKS = '''Blanks'''
+ENGLISH = '''English'''
+HANZI = '''Hanzi'''
+PINYIN = '''Pinyin'''
+COLUMNS = (
+    FK_PARENT,
+    MANUAL_LEVEL,
+    AUTO_LEVEL,
+    BLANKS,
+    ENGLISH,
+    HANZI,
+    PINYIN,
 )
-fk_parent_row = 0
-manual_level_row = 1
-auto_level_row = 2
-blanks_row = 3
-english_row = 4
-hanzi_row = 5
-pinyin_row = 6
 
 ioHelper = IoHelper()
 ioHelper.prepareAutoLevel()
@@ -59,16 +59,16 @@ for filename in input_file_names:
     ioHelper.verify_no_new_old_duplicates(df_new_input, df_existing_output)
 
     # get info for new input to output
-    df_new_input[columns[pinyin_row]] = df_new_input.apply(lambda row: ioHelper.pinyin_from_hanzi_googletrans(row), axis=1)
+    df_new_input[PINYIN] = df_new_input.apply(lambda row: ioHelper.pinyin_from_hanzi_googletrans(row), axis=1)
     time.sleep(sleep_time)
-    df_new_input[columns[hanzi_row]] = df_new_input.apply(lambda row: ioHelper.hanzi_with_spaces(row), axis=1)
-    df_new_input[columns[auto_level_row]] = df_new_input.apply(lambda row: ioHelper.auto_level(row), axis=1)
-    df_new_input[columns[manual_level_row]] = df_new_input.apply(lambda row: ioHelper.manual_level(row), axis=1)
+    df_new_input[HANZI] = df_new_input.apply(lambda row: ioHelper.hanzi_with_spaces(row), axis=1)
+    df_new_input[AUTO_LEVEL] = df_new_input.apply(lambda row: ioHelper.auto_level(row), axis=1)
+    df_new_input[MANUAL_LEVEL] = df_new_input.apply(lambda row: ioHelper.manual_level(row), axis=1)
 
     df_merged = pd.concat([df_existing_output, df_new_input], ignore_index=True, sort=False)
 
     # update output file
-    df_merged.to_csv(output_csv_filename, index=False, header=True, columns=columns)
+    df_merged.to_csv(output_csv_filename, index=False, header=True, columns=COLUMNS)
 
     # remove all data from input file
     df_empty = df_new_input[0:0]
@@ -86,74 +86,11 @@ os.remove(database_name)
 con = sqlite3.connect(database_name)
 cur = con.cursor()
 
-output_file_names = os.listdir(output_path)
-output_file_names.sort()
+dbHelper = DbHelper(cur, output_path, COLUMNS, FK_PARENT, MANUAL_LEVEL, AUTO_LEVEL, BLANKS, ENGLISH, HANZI, PINYIN)
+dbHelper.writeTranslations()
 
-db_in_mem = {}
 
-for file_idx, filename in enumerate(output_file_names):
-    is_input_to_primary_table = filename[0] == '0'
-    if is_input_to_primary_table:
-        table_name = 'translations'
-    else:
-        table_name = filename[filename.index('_') + 1:filename.index('.')]
 
-    output_csv_filename = '{}{}'.format(output_path, filename)
-    with open(output_csv_filename, 'rt') as fin:  # `with` statement available in 2.5+
-        # csv.DictReader uses first line in file for column headings by default
-        dr = csv.DictReader(fin)  # comma is default delimiter
-        to_db = [
-            (
-                i[('%s' % columns[0])],
-                i[('%s' % columns[1])],
-                i[('%s' % columns[2])],
-                i[('%s' % columns[3])],
-                i[('%s' % columns[4])],
-                i[('%s' % columns[5])],
-                i[('%s' % columns[6])],
-            )
-            for i in dr]
-
-    if not is_input_to_primary_table:
-        db_in_mem[table_name] = {}
-        for idx, row in enumerate(to_db):
-            english_value = row[english_row]
-            db_in_mem[table_name][english_value] = idx
-
-    fk_parent = to_db[0][0]
-    no_fk_ref = fk_parent == "" or fk_parent == '-1'
-    if no_fk_ref:
-        table_contents_fk_ref = ''
-        for idx in range(len(to_db)):
-            to_db[idx] = (-1,) + to_db[idx][1:]
-    else:
-        foreign_key_table_name = fk_parent[0:fk_parent.index('.')]
-        table_contents_fk_ref = ',FOREIGN KEY({}) REFERENCES {}(id)'.format(columns[0], foreign_key_table_name)
-        for idx in range(len(to_db)):
-            ref_split = str(to_db[idx][0]).split('.')
-            to_db[idx] = (db_in_mem[ref_split[0]][ref_split[1]] + 1,) + to_db[idx][1:]
-
-    if file_idx == 0 or not is_input_to_primary_table:
-
-        table_cont_new = '('
-        table_cont_new += '[id] INTEGER PRIMARY KEY,'
-        for col in columns[:3]:
-            table_cont_new += '[{}] INTEGER,'.format(col)
-        for col in columns[3:-1]:
-            table_cont_new += '[{}] text,'.format(col)
-        for col in columns[-1:]:
-            table_cont_new += '[{}] text'.format(col)
-        table_cont_new += table_contents_fk_ref
-        table_cont_new += ')'
-
-        contents = '''CREATE TABLE ''' + table_name + ''' ''' + table_cont_new
-        cur.execute(contents)
-
-    percent_s = ", ".join(['%s'] * len(columns))
-    question_mark = ", ".join(['?'] * len(columns))
-    insert_str = ("INSERT INTO " + table_name + " (" + percent_s + ") VALUES (" + question_mark + ");")
-
-    cur.executemany(insert_str % columns, to_db)
 
 con.commit()
 con.close()
